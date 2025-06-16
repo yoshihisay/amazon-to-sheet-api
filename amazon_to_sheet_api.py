c9e9637544c5cdedcbd74c0c540b89954a93a9f2
yoshihisay <113879411+yoshihisay@users.noreply.github.com>
2025-06-17 04:40:28 +0900

    Update amazon_to_sheet_api.py

--- amazon_to_sheet_api.py
+++ amazon_to_sheet_api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from python_amazon_paapi import AmazonAPI
+from amazon_paapi import AmazonAPI
 import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
